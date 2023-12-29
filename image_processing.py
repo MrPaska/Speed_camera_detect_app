c9d8f5d5b7cbc36b638778bdd386567b6274efbb
Paulius Bakšys <75304721+MrPaska@users.noreply.github.com>
2023-12-29 18:35:51 +0200

    Update image_processing.py

--- image_processing.py
+++ image_processing.py
@@ -1,15 +1,50 @@
+import os
 import cv2
-import matplotlib.pyplot as plt
 import easyocr
+import matplotlib.pyplot as plt
+from itertools import count
+from snackbar import alert_window
+
+reader = easyocr.Reader(["en"])
+zone_dict = dict()
+i = count(start=1)
 
 
-def read_zone():
-    zona_img = cv2.imread("zone_cropped.jpg")
-    gray_img = cv2.cvtColor(zona_img, cv2.COLOR_BGRA2GRAY)
+def process_img(saved_photos):
+    for saved_photo in saved_photos:
+        path = os.path.join("./", saved_photo)
+        print(path)
+        zona_img = cv2.imread(path)
+        gray_img = cv2.cvtColor(zona_img, cv2.COLOR_BGRA2GRAY)
+        read_text(gray_img)
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+        dilated_img = cv2.dilate(gray_img, kernel, iterations=1)
+        read_text(dilated_img)
+    clean_text()
 
-    reader = easyocr.Reader(["en"])
-    result = reader.readtext(gray_img)
-    text = result[0][1]
-    cleaning = ''.join(char for char in text if char.isalnum())
-    print(cleaning)
-    return cleaning
+
+def read_text(image):
+    results = reader.readtext(image)
+    label = results[0][1]
+    conf = round(results[0][2], 2)
+    zone_dict[next(i)] = (label, conf)
+
+
+def clean_text():
+    max_val = 0
+    max_label = None
+    for key, values in zone_dict.items():
+        if max_val < values[1]:
+            max_val = values[1]
+            max_label = values[0]
+            cleaned_label = ''.join(char for char in max_label if char.isalnum())
+
+    print(f"Label: {cleaned_label} max_value: {max_val}")
+    alert_window("Aptiktas vidutinio greičio matuoklis!", cleaned_label)
+
+
+# process_img(["zone_cropped_1.jpg", "zone_cropped_2.jpg", "zone_cropped_3.jpg"])
+# zone_label = clean_text()
+# print(zone_dict)
+# print(f"Zona nustatyta: {zone_label}")
+
