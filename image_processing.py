0448e3b2826b239115de1172ad3fffea181ae700
Paulius Bakšys <75304721+MrPaska@users.noreply.github.com>
2023-12-29 21:59:00 +0200

    Update image_processing.py

--- image_processing.py
+++ image_processing.py
@@ -1,13 +1,12 @@
 import os
 import cv2
 import easyocr
-import matplotlib.pyplot as plt
 from itertools import count
 from snackbar import alert_window
 
 reader = easyocr.Reader(["en"])
 zone_dict = dict()
-i = count(start=1)
+i = 0
 
 
 def process_img(saved_photos):
@@ -24,27 +23,30 @@ def process_img(saved_photos):
 
 
 def read_text(image):
-    results = reader.readtext(image)
-    label = results[0][1]
-    conf = round(results[0][2], 2)
-    zone_dict[next(i)] = (label, conf)
+    global i
+    i += 1
+    try:
+        results = reader.readtext(image)
+        label = results[0][1]
+        conf = round(results[0][2], 2)
+        zone_dict[i] = (label, conf)
+    except Exception as e:
+        print(e)
 
 
 def clean_text():
+    global i
     max_val = 0
     max_label = None
+    cleaned_label = None
     for key, values in zone_dict.items():
         if max_val < values[1]:
             max_val = values[1]
             max_label = values[0]
             cleaned_label = ''.join(char for char in max_label if char.isalnum())
 
+    print(zone_dict)
+    i = 0
+    zone_dict.clear()
     print(f"Label: {cleaned_label} max_value: {max_val}")
     alert_window("Aptiktas vidutinio greičio matuoklis!", cleaned_label)
-
-
-# process_img(["zone_cropped_1.jpg", "zone_cropped_2.jpg", "zone_cropped_3.jpg"])
-# zone_label = clean_text()
-# print(zone_dict)
-# print(f"Zona nustatyta: {zone_label}")
-
