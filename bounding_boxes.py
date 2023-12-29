38706726f01bdf538aa47eb88316d49b0b60b205
Paulius Bakšys <75304721+MrPaska@users.noreply.github.com>
2023-12-29 21:59:47 +0200

    Update bounding_boxes.py

--- bounding_boxes.py
+++ bounding_boxes.py
@@ -1,19 +1,20 @@
 from ultralytics import YOLO
 import cvzone
 import math
-from statistics import post_statistics
 from datetime import datetime, timedelta
 import play_voice
 from snackbar import alert_window
 import cv2
 from image_processing import process_img
-from itertools import count
 
 classes = ["momentinis", "vidutinis", "zona"]
 model = YOLO("best2_openvino_model", task='detect')
 new_datetime = datetime.now()
-i = 0
+current_datetime = None
+vidutinis = False
 saved_photos = []
+i = 0
+
 buttons = {
     "momentinis": None,
     "vidutinis": None,
@@ -28,10 +29,8 @@ def params(moment, avg, zone):
 
 
 def recognition(frames, user_id):
-    global new_datetime, i
-    vidutinis = False
+    global new_datetime, i, vidutinis, current_datetime
     current_datetime = datetime.now()
-
     if current_datetime >= new_datetime:
         results = model(frames)
         for r in results:
@@ -46,27 +45,26 @@ def recognition(frames, user_id):
                 print(box)
 
                 if conf >= 0.8 and classes[cls] == "momentinis":
-                    play_voice.play_voice(0)
-                    alert_window("Aptiktas momentinis greičio matuoklis!")
                     new_datetime = current_datetime + timedelta(seconds=5)
+                    play_voice.play_voice(0)
+                    alert_window("Aptiktas momentinis greičio matuoklis!", None)
+                    break
                 if conf >= 0.8 and classes[cls] == "vidutinis":
                     vidutinis = True
                 if conf >= 0.8 and classes[cls] == "zona" and vidutinis is True:
                     i += 1
                     print(vidutinis)
                     get_zona_box(frames, x1, x2, y1, y2, i)
-                    new_datetime = current_datetime + timedelta(seconds=.5)
+                    new_datetime = current_datetime + timedelta(seconds=.3)
                     print(i)
-                    if i >= 3:
-                        process_img(saved_photos)
-                        saved_photos.clear()
-                        play_voice.play_voice(1)
-                        i = 0
-                        new_datetime = current_datetime + timedelta(seconds=2)
-                    else:
-                        pass
+                    if i == 5:
+                        shutdown()
+                        break
                 cvzone.putTextRect(frames, f'{cls} {conf}', (max(0, x1), max(35, y1)), scale=0.7, thickness=1)
-
+            if len(boxes) == 0 and vidutinis:
+                print("boxes = 0")
+                shutdown()
+                break
         return frames
 
 
@@ -80,3 +78,12 @@ def get_zona_box(frames, x1, x2, y1, y2, n):
     except Exception as e:
         print(f"failed to save photo {e}")
 
+
+def shutdown():
+    global new_datetime, i, vidutinis, current_datetime
+    new_datetime = current_datetime + timedelta(seconds=5)
+    process_img(saved_photos)
+    saved_photos.clear()
+    play_voice.play_voice(1)
+    i = 0
+    vidutinis = False
